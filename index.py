6b128f4b24bea1416f27735aef9f623937e1d682
Zoxir Rustamov <zoxabtw77@gmail.com>
2021-12-11 20:00:27 +0500

    lesson17

---
+++ index.py
@@ -0,0 +1,47 @@
+#while 
+#for 
+#lamde fx
+#functions
+
+# int
+# float
+# list
+# bool
+# tumple
+# dict
+# set
+
+
+# for letter in 'hello':
+#     print(letter) 
+
+# for number in [10, 20, 30]:
+#     print(number)
+
+# for text in ['Elbek, Zokhir, Shaxa']:
+#     print(text)
+
+
+# for names in ['Elbek, Zokhir, Shaxa']:
+#     print(names)
+
+# names = ['Zokhir']
+# names.append
+
+
+# dict = {
+#     0: 'Zokhir',
+#     1: 'Shaxa'
+# }
+# print(dict.get(2))
+
+
+
+# print(dict)
+
+def format_tel(tel):
+    pass
+
+format_tel('+998997752003')
+
+print('(%s) %s-%s' % tuple.findall(r'\d{4}$|\d{3}', '+998997752003'))
